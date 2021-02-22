e05612bb4cc06c5d6ab819db9ebf943dbaffdc88
Shiva-OCS <shiva.bernhard.ext@orange.com>
2021-02-22 10:49:51 +0100

    inserted GPL license header

--- models/src/main/java/fr/shining_cat/everyday/models/reward/Level.kt
+++ models/src/main/java/fr/shining_cat/everyday/models/reward/Level.kt
@@ -1,3 +1,20 @@
+/*
+ * Copyright (c) 2021. Shining-cat - Shiva Bernhard
+ * This file is part of Everyday.
+ *
+ *     Everyday is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, version 3 of the License.
+ *
+ *     Everyday is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with Everyday.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package fr.shining_cat.everyday.models.reward
 
 // TODO : we need 6 versions of FLOWER and MOUTH + 7 versions of LEGS, ARMS, and EYES (6 normal and 1 "off" version : legs or arms folded, eyes closed), the "off" version of HORNS is simply empty
