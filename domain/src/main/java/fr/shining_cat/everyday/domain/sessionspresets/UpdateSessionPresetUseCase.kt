a2e26a140a929f71fcc649dd1ffafb0d38fe3e26
Shiva <shiva.bernhard@shining-cat.fr>
2021-03-10 11:41:30 +0100

    update session preset usecase + tests

--- domain/src/main/java/fr/shining_cat/everyday/domain/sessionspresets/UpdateSessionPresetUseCase.kt
+++ domain/src/main/java/fr/shining_cat/everyday/domain/sessionspresets/UpdateSessionPresetUseCase.kt
@@ -1,21 +1,20 @@
 package fr.shining_cat.everyday.domain.sessionspresets
 
-import fr.shining_cat.everyday.commons.Constants
 import fr.shining_cat.everyday.commons.Constants.Companion.ERROR_CODE_DATABASE_OPERATION_FAILED
-import fr.shining_cat.everyday.commons.Constants.Companion.ERROR_MESSAGE_DELETE_FAILED
+import fr.shining_cat.everyday.commons.Constants.Companion.ERROR_MESSAGE_UPDATE_FAILED
 import fr.shining_cat.everyday.commons.Logger
 import fr.shining_cat.everyday.domain.Result
 import fr.shining_cat.everyday.models.SessionPreset
 import fr.shining_cat.everyday.repository.Output
 import fr.shining_cat.everyday.repository.repo.SessionPresetRepository
 
-class DeleteSessionPresetUseCase(
+class UpdateSessionPresetUseCase(
     private val sessionPresetRepository: SessionPresetRepository,
     private val logger: Logger
 ) {
 
     suspend fun execute(sessionPreset: SessionPreset): Result<Int> {
-        val output = sessionPresetRepository.delete(sessionPreset)
+        val output = sessionPresetRepository.update(sessionPreset)
         return if (output is Output.Success) {
             //this usecase only handle single item deletion
             if (output.result == 1) {
@@ -24,8 +23,8 @@ class DeleteSessionPresetUseCase(
             else {
                 Result.Error(
                     ERROR_CODE_DATABASE_OPERATION_FAILED,
-                    ERROR_MESSAGE_DELETE_FAILED,
-                    Exception(ERROR_MESSAGE_DELETE_FAILED)
+                    ERROR_MESSAGE_UPDATE_FAILED,
+                    Exception(ERROR_MESSAGE_UPDATE_FAILED)
                 )
             }
         }
